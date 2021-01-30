b4253f06995165c7db0d824109e9f72704ad8b3d
Juyong Hong <juyong3525@gmail.com>
2021-01-30 20:24:46 +0900

    Added pymymongo insert

--- traning/pymongo_tutorial.py
+++ traning/pymongo_tutorial.py
@@ -5,6 +5,9 @@ knowledge = conn.knowledge
 
 knowledge_it = knowledge.it
 
-post = {"author": "Mike", "text": "My first blog post!",
-        "tags": ["mongodb", "python", "pymongo"]}
-knowledge_it.insert_one(post)
+knowledge_it.insert_one(
+    {
+        "author": "Pong",
+        "age": 25
+    }
+)
